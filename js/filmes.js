37d2b63222ef2a143299956f5aedcfbe166a7c29
NPS <nicolaspetri18@gmail.com>
2024-03-12 10:50:03 -0300

    senai, para de bloquear meu ip

--- js/filmes.js
+++ js/filmes.js
@@ -1,19 +1,37 @@
 export async function getFilmes() {
-    const url = 'http://10.107.134.43:8080/v2/acme/filmes'
+    const url = 'http://localhost:8081/v2/acmefilmes/filmes'
 
     const response = await fetch(url)
     const filmes = await response.json()
 
     return filmes.filmes
+
 }
+
 
 export async function getFilme(id) {
     const idFilme = id
 
-    const url = `http://10.107.134.43:8080/v2/acme/filme/${idFilme}`
+    const url = `http://localhost:8081/v2/acmefilmes/filme/${idFilme}`
 
     const response = await fetch(url)
     const filme = await response.json()
 
     return filme.filme[0]
 }
+
+// export async function postFilme(filme) {
+//     const url = `http://10.107.134.41:8081/v2/acmefilmes/filme`
+
+//     const options = {
+//         method: 'POST',
+//         headers: {
+//             'Content-type': 'application/json'
+//         },
+//         body: JSON.stringify(filme)
+//     }
+
+//     const response = await fetch(url, options)
+
+//     return response.ok
+// }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 import { getFilmes, getFilme } from "./filmes.js"
 
+async function preencherCards() {
+    const container = document.querySelector('body')
+
+    try {
+        const filmes = await getFilmes()
+        if (filmes && filmes.length > 0) {
+            filmes.forEach(filme => {
+                const card = criarCards(filme)
+                container.appendChild(card)
+            })
+        } else {
+            console.error("Nenhum filme encontrado")
+        }
+    } catch (error) {
+        console.error("Erro ao preencher os cards:", error)
+    }
+}
+
+
 function criarCards(filme) {
     const card = document.createElement('div')
     const titulo = document.createElement('h2')
@@ -14,18 +33,16 @@ function criarCards(filme) {
     return card
 }
 
-async function preencherCards() {
-
-    const container = document.querySelector('body')
-
-    const filmes = await getFilmes()
-
-    filmes.forEach(filme => {
-        const card = criarCards(filme)
-        container.appendChild(card)
-    })
+const filme = {
+    "nome": "Teste Post, HTML",
+    "sinopse": "Testeeee",
+    "duracao": "02:35:00",
+    "data_lancamento": "2024-03-12",
+    "data_relancamento": "",
+    "foto_capa": "https://exemplo.com/foto_vale.jpg",
+    "valor_unitario": 52.12
 }
 
-preencherCards()
-
+// await postFilme(filme)
+await preencherCards()
 console.table(await getFilme(2))
